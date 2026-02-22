8a7ceae1c6f76cc563e41e5f63a821fe71c58a06
TejaswiTwilight12 <tejaswitwilight12@gmail.com>
2026-02-21 19:58:18 -0500

    Fix frontend static path

--- backend/server.js
+++ backend/server.js
@@ -136,7 +136,8 @@ app.get("/api", (req, res) => {
 });
 
 // ===== Serve Frontend =====
-const publicPath = path.join(__dirname, "public");
+const publicPath = path.join(__dirname, "../frontend/dist");
+
 app.use(express.static(publicPath));
 
 app.get("*", (req, res) => {
