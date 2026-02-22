1b8e1a68ed41528e10a1c81abaa43828a34240bd
TejaswiTwilight12 <tejaswitwilight12@gmail.com>
2026-02-22 00:57:00 -0500

    Fix broken API route declarations

--- backend/server.js
+++ backend/server.js
@@ -140,21 +140,9 @@ const publicPath = path.join(__dirname, "../frontend/dist");
 
 app.use(express.static(publicPath));
 
-app.get("*", (req, res, next) => {
-  // If request starts with API route, skip
-  if (
-    req.path.startsWith("/admissions") ||
-    req.path.startsWith("/beds") ||
-    req.path.startsWith("/staffing") ||
-    req.path.startsWith("/alerts") ||
-    req.path.startsWith("/forecast")
-  ) {
-    return next();
-  }
-
+app.get("*", (req, res) => {
   res.sendFile(path.join(publicPath, "index.html"));
 });
-
 
 // ===== 404 =====
 //app.use((req, res) => {
